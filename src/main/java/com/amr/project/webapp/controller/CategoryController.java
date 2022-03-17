e433033790e81941d285f2db97ae93328e8df8c8
Evgeniy <zhel186@mail.ru>
2022-03-17 15:37:29 +0300

    add category rest

--- src/main/java/com/amr/project/webapp/controller/CategoryController.java
+++ src/main/java/com/amr/project/webapp/controller/CategoryController.java
@@ -13,12 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -53,9 +51,10 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "Found the order", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = CategoryDto.class))}),
             @ApiResponse(responseCode = "404", description = "Order not found", content = @Content)})
     @GetMapping("/categories/{id}")
-    public ResponseEntity<CategoryDto> getCategory(@PathVariable(name = "id") Long id) {
+    public ResponseEntity<Optional<CategoryDto>> getCategory(@PathVariable(name = "id") Long id) {
         CategoryDto categoryDto = categoryService.getCategoryById(id);
         logger.info(GET_CATEGORY_LOG, categoryDto.getId());
-        return new ResponseEntity<>(categoryDto, HttpStatus.OK);
+        return new ResponseEntity<>(Optional.of(categoryDto), HttpStatus.OK);
     }
+
 }
