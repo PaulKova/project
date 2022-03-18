6026131a95c9a65bfbffe7c932f5fa53a0d80829
Rozelux <rozelux@yandex.ru>
2022-03-18 20:33:41 +0300

    Created OrderRestController

--- src/main/java/com/amr/project/dao/ItemRepository.java
+++ src/main/java/com/amr/project/dao/ItemRepository.java
@@ -14,4 +14,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     //TODO протестировать, что запрос работает корректно (поиск по ключевому слову в имени вне зависимости от количества символов до и после
     @Query("select i from Item i where i.name like concat('%', :searchString, '%') order by i.rating desc")
     List<Item> searchItemsByNameSortedByRatingDesc (@Param("searchString")String searchString);
+
+
 }
