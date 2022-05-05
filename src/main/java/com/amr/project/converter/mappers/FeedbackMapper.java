3d287766c1c21695ed3d7130c3e84ce869e5432f
askhelen <askhelen@yandex.ru>
2022-05-05 20:15:48 +0300

    изменение мапперов

--- src/main/java/com/amr/project/converter/mappers/FeedbackMapper.java
+++ src/main/java/com/amr/project/converter/mappers/FeedbackMapper.java
@@ -1,15 +1,27 @@
 package com.amr.project.converter.mappers;
 
+import com.amr.project.converter.CycleAvoidingMappingContext;
 import com.amr.project.converter.MapperInterface;
 import com.amr.project.model.dto.FeedbackDto;
 import com.amr.project.model.entity.Feedback;
-import org.mapstruct.Builder;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(builder = @Builder(disableBuilder = true), componentModel = "spring", uses = {ShopMapper.class, UserMapper.class})
 public interface FeedbackMapper extends MapperInterface<FeedbackDto, Feedback> {
+
+    @Override
+    @Mappings({
+            @Mapping(target = "userId", source = "entity.user.id")
+    })
+    FeedbackDto toDto(Feedback entity,CycleAvoidingMappingContext cycleAvoidingMappingContext);
+
+    @Override
+    @Mappings({
+            @Mapping(target = "user.id", source = "dto.userId")
+    })
+    Feedback toEntity(FeedbackDto dto, CycleAvoidingMappingContext cycleAvoidingMappingContext);
 
 }
--- src/main/java/com/amr/project/converter/mappers/ReviewMapper.java
+++ src/main/java/com/amr/project/converter/mappers/ReviewMapper.java
@@ -1,15 +1,39 @@
 package com.amr.project.converter.mappers;
 
+import com.amr.project.converter.CycleAvoidingMappingContext;
 import com.amr.project.converter.MapperInterface;
+import com.amr.project.model.dto.MessageDto;
 import com.amr.project.model.dto.ReviewDto;
+import com.amr.project.model.entity.Message;
 import com.amr.project.model.entity.Review;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(builder = @Builder(disableBuilder = true), componentModel = "spring", uses = {UserMapper.class, ShopMapper.class, ItemMapper.class})
 public interface ReviewMapper extends MapperInterface<ReviewDto, Review> {
+
+    @Override
+    @Mappings({
+            @Mapping(target = "userId", source = "entity.user.id"),
+            @Mapping(target = "itemId", source = "entity.item.id"),
+            @Mapping(target = "shopId", source = "entity.shop.id"),
+            @Mapping(target = "userName", source = "entity.user.username")
+    })
+    ReviewDto toDto(Review entity, CycleAvoidingMappingContext cycleAvoidingMappingContext);
+
+
+    @Override
+    @Mappings({
+            @Mapping(target = "user.id", source = "dto.userId"),
+            @Mapping(target = "item.id", source = "dto.itemId"),
+            @Mapping(target = "shop.id", source = "dto.shopId"),
+            @Mapping(target = "user.username", source = "dto.userName")
+    })
+    Review toEntity(ReviewDto dto, CycleAvoidingMappingContext cycleAvoidingMappingContext);
 
 }
